50bed4d4cbda95c21a524e557283917c42d623c3
rwkama1 <rwkamandriw@gmail.com>
2022-08-25 14:07:38 -0300

    modules

---
+++ .next/static/qsH7jn2utaMSYDTGuKBvJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0921b0d1c1a1f5a9.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
