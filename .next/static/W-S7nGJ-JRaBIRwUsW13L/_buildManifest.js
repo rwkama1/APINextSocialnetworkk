1e79ab918acb8191caf1dd999cadb329332d30f5
rwkama1 <rwkamandriw@gmail.com>
2022-08-24 23:07:04 -0300

    b

--- .next/static/W-S7nGJ-JRaBIRwUsW13L/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-abbe9d07c3ff8173.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
